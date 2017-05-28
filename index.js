81090c2b63b5f16c7e9f9733589c9623a83c0685
imears <maxwellmears@gmail.com>
2017-05-28 11:52:06 -0700

    Create basic stress test for Rails API with basic auth

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-const rp = require('request-promise');
+const rp       = require('request-promise');
+const inquirer = require('inquirer');
 
-const options = {
-  uri: 'http://localhost:3000/v1/users',
-  json: true
-};
+const methods     = ['GET', 'PUT', 'POST', 'PATCH', 'DELETE'];
+const dataMethods = ['PUT', 'POST', 'PATCH'];
 
-const promises = [];
-const attempts = 1000;
-const callback = (users) => console.log(users);
+const questions = [
+  { message: 'enter test uri: ',  name: 'uri',            type: 'input',   default: 'http://localhost:3000/v1/users' },
+  { message: 'iterations? ',      name: 'iterations',     type: 'input',   default: 100 },
+  { message: 'method? ',          name: 'method',         type: 'list',    default: 0, choices: methods },
+  { message: 'enter data: ',      name: 'body',           type: 'input',   when: (answers) => dataMethods.includes(answers.method) },
+  { message: 'include headers? ', name: 'includeHeaders', type: 'confirm', default: true },
+  { message: 'enter headers: ',   name: 'headers',        type: 'input',   when: (answers) => answers.includeHeaders, default: {'Authorization': 'Token realm="weZgc42G2UNHqhzx29nQrAtt"'} },
+  { message: 'json response? ',   name: 'json',           type: 'confirm', default: true },
+];
 
-for (i = 0; i < attempts; i++) {
-  const promise = rp(options).then(callback);
-  promises.push(promise);
+function stressTest(options, promises = []) {
+  console.time('elapsed-time');
+
+  for (i = 0; i < options.iterations; i += 1) {
+    promises.push(rp(options).then(console.log).catch(console.log));
+  }
+
+  Promise.all(promises).then((values) => console.timeEnd('elapsed-time'));
 }
 
-Promise.all(promises).then((values) => console.log('done'));
+inquirer.prompt(questions).then(stressTest);
